442f722deb495c294b100a0c8bd188e55d1707e5
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-08-12 13:55:00 -0600

    Completed step 2 of importing ProductContext and wrapping the components in the provider

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
 //contexts
-import { ProductContext } from './contexts/ProductContext';
+import { ProductContext }  from './contexts/ProductContext';
 
 function App() {
 	const [products] = useState(data);
@@ -19,26 +19,19 @@ function App() {
 	};
 
 	return (
-		<div className="App">
-			<Navigation cart={cart} />
+		<ProductContext.Provider value={{ products, addItem }}>
+			<div className="App">
+				<Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+				{/* Routes */}
+				<Route exact path="/" component={Products} />
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
+				<Route
+					path="/cart"
+					render={() => <ShoppingCart cart={cart} />}
+				/>
+			</div>
+		</ProductContext.Provider>
 	);
 }
 

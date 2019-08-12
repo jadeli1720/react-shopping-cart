4cfb65cc6e769fadd840043e36ac112cd0215b2d
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-08-12 14:28:50 -0600

    Completed step 5: refactored navigation.js and shoppingcart.js

--- src/App.js
+++ src/App.js
@@ -21,17 +21,19 @@ function App() {
 
 	return (
 		<ProductContext.Provider value={{ products, addItem }}>
-			<div className="App">
-				<Navigation cart={cart} />
+			<CartContext.Provider value={cart}>
+				<div className="App" >
+					<Navigation cart={cart} />
 
-				{/* Routes */}
-				<Route exact path="/" component={Products} />
+					{/* Routes */}
+					<Route exact path="/" component={Products} />
 
-				<Route
-					path="/cart"
-					render={() => <ShoppingCart cart={cart} />}
-				/>
-			</div>
+					<Route
+						path="/cart"
+						render={() => <ShoppingCart cart={cart} />}
+					/>
+				</div>
+			</CartContext.Provider>
 		</ProductContext.Provider>
 	);
 }
